3aefb9f5e2de6ee3d95222a49ab939922322973d
AsitSwain2002 <asitswain7327@gmil.com>
2024-01-21 11:40:53 +0530

    created weather app

---
+++ script.js
@@ -0,0 +1,56 @@
+let time = document.querySelector(".in-box1");
+let cell = document.querySelector(".in-cel");
+let wind = document.querySelector(".wind");
+let btn = document.querySelector(".btn");
+let text = document.querySelector("#text");
+let description = document.querySelector(".description");
+let logo = document.querySelector(".logo");
+let pressure = document.querySelector(".pressure");
+let precip = document.querySelector(".precip");
+let wind_dir = document.querySelector(".wind_dir");
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   setInterval(showTime, 1000);
+// });
+
+// const showTime = () => {
+//   const date = new Date();
+//   const hr = date.getHours();
+//   const min = date.getMinutes();
+//   time.innerHTML = `${hr}:${min}`;
+// };
+btn.addEventListener("click", () => {
+  const a = text.value;
+  if (a === "") {
+    alert("Enter City name");
+  }
+  // else{
+  const url = `https://api.weatherapi.com/v1/current.json?q=${a}&key=aba93a92e3234fc1a02111751242001`;
+
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      cell.innerHTML = data.current.temp_c;
+      wind.innerHTML = data.current.wind_kph;
+      description.innerHTML = data.current.condition.text;
+      const iconUrl = data.current.condition.icon;
+      document.querySelector(".logo-img").src = iconUrl;
+      pressure.innerHTML = data.current.pressure_mb;
+      precip.innerHTML = data.current.precip_mm;
+      time.innerHTML = data.location.localtime;
+      wind_dir.innerHTML = data.current.wind_dir;
+    })
+    .catch((error) => {
+      // You can provide user-friendly error handling here.
+      // For example, display an error message on the page.
+      description.innerHTML = "Error fetching weather data.";
+    });
+  // }
+});
